9c5493596363fb85f218c4b2c2a8475806503f33
reallaksh19 <reallaksh19@gmail.com>
2026-09-10 20:43:39 +0400

    Core2: stage package-validator entrypoint copy

---
+++ Grade 9/skills/grade9-core2-publisher/scripts/validate_core2_package_entrypoint_new.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Independent Core (2) package validator entrypoint."""
+from validate_core2_package_structured_impl import main
+
+if __name__ == "__main__":
+    raise SystemExit(main())
